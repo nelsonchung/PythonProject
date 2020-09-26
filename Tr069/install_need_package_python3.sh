6f8e0f2a7ef0156ecbbc05446ec686a3bbf882f2
Nelson Chung <chihchun.chung@gmail.com>
2020-09-26 17:35:45 +0800

    Add install package script

---
+++ Tr069/install_need_package_python3.sh
@@ -0,0 +1,5 @@
+sudo apt install python3-pip
+pip3 install numpy
+pip3 install openpyxl
+pip3 install client
+pip3 install suds
---
+++ Tr069/install_pip_ubuntu2004.sh
@@ -0,0 +1,5 @@
+sudo add-apt-repository universe
+sudo apt update
+
+curl https://bootstrap.pypa.io/get-pip.py --output get-pip.py
+sudo python2 get-pip.py
